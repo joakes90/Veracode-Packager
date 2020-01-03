851eeac23ccfff6f61a2ba4e939cf3eb7b5249cd
Justin Oakes <Justin.Oakes@loves.com>
2020-01-03 13:20:08 -0600

    fixed veracode issues with script

--- veracode_packager.sh
+++ veracode_packager.sh
@@ -14,9 +14,9 @@ cd "$ARCHIVE"
 ## Creating the Veracode Bitcode archive
 mv Products/Applications ./Payload
 rm -r Products
-zip -r LovesConnect.bca ./*
+zip -r $FILEPATH ./*
 
 ## Upload LovesConnect.bca to Veracode
 curl -o veracode-wrapper.jar https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/19.3.5.7/vosp-api-wrappers-java-19.3.5.7.jar
-java -jar veracode-wrapper.jar -action uploadandscan -vid b6a1af53e81028c9e854d0f7636a51cc -vkey 4ef0ff8fe7cfa53f828398b80952625bab31e6c910921fc32949834a405520a4b5ed215ebd587a8186d4d562366cb117892832bd1f2745452c2f452739aa375a -appid 371192 -appname "iOS App" -autoscan true -platform iOS -createprofile false -version $(date "+%Y%m%d%H%M%S") -filepath ./LovesConnect.bca
+java -jar veracode-wrapper.jar -action uploadandscan -vid $VERACODE_ID -vkey $VERACODE_KEY -appid $APP_ID -appname $APP_NAME -autoscan true -platform iOS -createprofile false -version $(date "+%Y%m%d%H%M%S") -filepath $FILE_PATH
 
